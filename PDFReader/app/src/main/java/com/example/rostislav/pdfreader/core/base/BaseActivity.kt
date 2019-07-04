f3fc7c15b94d7f9847cb778dbdb51fd131d5e90a
Rostislav <rostik913@gmail.com>
2019-07-04 17:18:12 +0300

    update code style 3

--- PDFReader/app/src/main/java/com/example/rostislav/pdfreader/core/base/BaseActivity.kt
+++ PDFReader/app/src/main/java/com/example/rostislav/pdfreader/core/base/BaseActivity.kt
@@ -1,7 +1,7 @@
 package com.example.rostislav.pdfreader.core.base
 
 import android.os.Bundle
-import android.support.v7.app.AppCompatActivity
+import androidx.appcompat.app.AppCompatActivity
 import com.example.rostislav.pdfreader.core.App
 import com.example.rostislav.pdfreader.feature.main.Presenter
 
