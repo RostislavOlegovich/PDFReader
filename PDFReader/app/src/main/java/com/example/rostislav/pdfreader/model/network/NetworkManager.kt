ee95412f5d9c25f35a498f50995b12c270575197
Rostislav <rostik913@gmail.com>
2019-07-24 09:51:26 +0300

    UPD: network package

--- PDFReader/app/src/main/java/com/example/rostislav/pdfreader/model/network/NetworkManager.kt
+++ PDFReader/app/src/main/java/com/example/rostislav/pdfreader/model/network/NetworkManager.kt
@@ -2,52 +2,19 @@ package com.example.rostislav.pdfreader.model.network
 
 import okhttp3.OkHttpClient
 import okhttp3.Request
-import okhttp3.ResponseBody
-import okio.*
 
 class NetworkManager : Network {
 
     override fun downloadFromNetwork(url: String, progressCallback: ((Long) -> Unit)?): ByteArray {
         val client = (OkHttpClient.Builder().addNetworkInterceptor {
-            val original = it.proceed(it.request())
-            original.newBuilder()
-                .body(ProgressResponseBody(original.body!!, progressCallback))
+            val originalResponse = it.proceed(it.request())
+            originalResponse.newBuilder()
+                .body(ProgressResponseBody(originalResponse.body!!, progressCallback))
                 .build()
         }).build()
         val request = Request.Builder()
             .url(url)
             .build()
         return client.newCall(request).execute().body!!.bytes()
-    }
-
-    class ProgressResponseBody(
-        val responseBody: ResponseBody,
-        var progressCallback: ((bytesDownloaded: Long) -> Unit)?
-    ) :
-        ResponseBody() {
-
-        private var bufferedSource: BufferedSource? = null
-
-        override fun contentLength() = responseBody.contentLength()
-
-        override fun contentType() = responseBody.contentType()
-
-        override fun source(): BufferedSource {
-            return bufferedSource ?: source(responseBody.source()).buffer()
-        }
-
-        private fun source(source: Source): Source {
-
-            var totalBytesRead: Long = 0
-
-            return object : ForwardingSource(source) {
-                override fun read(sink: Buffer, byteCount: Long): Long {
-                    val bytesRead = super.read(sink, byteCount)
-                    totalBytesRead += if (bytesRead != -1L) bytesRead else 0
-                    progressCallback!!.invoke((totalBytesRead * 100).div(responseBody.contentLength()))
-                    return bytesRead
-                }
-            }
-        }
     }
 }
---
+++ PDFReader/app/src/main/java/com/example/rostislav/pdfreader/model/network/ProgressResponseBody.kt
@@ -0,0 +1,30 @@
+package com.example.rostislav.pdfreader.model.network
+
+import okhttp3.ResponseBody
+import okio.*
+
+class ProgressResponseBody(val responseBody: ResponseBody, var progressCallback: ((bytesDownloaded: Long) -> Unit)?) :
+    ResponseBody() {
+    private var bufferedSource: BufferedSource? = null
+
+    override fun contentLength() = responseBody.contentLength()
+
+    override fun contentType() = responseBody.contentType()
+
+    override fun source(): BufferedSource {
+        return bufferedSource ?: source(responseBody.source()).buffer()
+    }
+
+    private fun source(source: Source): Source {
+        var totalBytesRead: Long = 0
+
+        return object : ForwardingSource(source) {
+            override fun read(sink: Buffer, byteCount: Long): Long {
+                val bytesRead = super.read(sink, byteCount)
+                totalBytesRead += if (bytesRead != -1L) bytesRead else 0
+                progressCallback!!.invoke((totalBytesRead * 100).div(responseBody.contentLength()))
+                return bytesRead
+            }
+        }
+    }
+}
